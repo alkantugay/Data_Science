fd7b6888f0c6113745d299e438a8cc5ef5a2b87d
Tugay <51509025+alkantugay@users.noreply.github.com>
2020-03-23 14:09:09 +0300

    Update numpyBasics.py

--- Python Notes (to Remember)/numpyBasics.py
+++ Python Notes (to Remember)/numpyBasics.py
@@ -97,3 +97,71 @@ print(array1[:,1])  # Satırlardan hepsini al, sütunlardan sadece 1. sütunu al
 print(array1[1,1:4])  # sadece 1. satırı al, 1. 2. ve 3. sutunu al
 
 print(array1[-1,:])  #en son satırı al ve tüm sutunları al
+
+#%%  Shape Manipulation
+
+import numpy as np
+
+array = np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+#flatten
+
+a = array.ravel()  ## Arrayi düz hale yani vektör haline getirir
+
+array2 = a.reshape(3,3)
+
+arrayT = array2.T  # array2 nin transpozunu aldık
+
+print(arrayT.shape)
+
+array5 = np.array([[1,2],[3,4],[4,5]])
+
+#%%  Stacking arrays
+
+import numpy as np
+
+arr1 = np.array([[1,2],[3,4]])
+arr2 = np.array([[-1,-2],[-3,-4]])
+
+
+# vertical
+# array([[1, 2],
+#        [3, 4]])
+# array([[-1, -2],
+#        [-3, -4]])
+
+arr3 = np.vstack((arr1,arr2))
+
+# horizontal
+# array([[1, 2],[-1, -2],
+#        [3, 4][-3, -4]])
+
+arr4 = np.hstack((arr1,arr2))
+
+print(arr3)
+print(arr4)
+
+#%% convert and copy
+
+import numpy as np
+
+liste = [1,2,3,4]   # list
+
+array = np.array(liste)   # np.array
+
+
+liste2 = list(array)
+
+a = np.array([5,6,7])
+
+b = a
+b[0] = 10  # b[0] = 10 dersek sadece b nin değil a array inin de değişmiş olduğunu görürüz
+c = a
+
+"""  numpy array memeory de bir değer olarak değil alan olarak depolanıyor.
+bu yüzden a b ya da c yi değiştirdiğimiz de hepsi değişiyor. Çözümü tabiki de var : """
+
+d = a.copy()
+
+f = d.copy()
+
