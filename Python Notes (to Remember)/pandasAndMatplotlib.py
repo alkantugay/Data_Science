112759a879fe2df3509a27b7b19d1250907b6b0f
Tugay <51509025+alkantugay@users.noreply.github.com>
2020-03-24 15:33:41 +0300

    Add files via upload

--- Python Notes (to Remember)/pandasAndMatplotlib.py
+++ Python Notes (to Remember)/pandasAndMatplotlib.py
@@ -4,7 +4,6 @@ Created on Mon Mar 23 17:23:00 2020
 
 @author: alkan
 """
-###!!! iris.csv dosyasını indirmeyi unutma!!!
 
 # matplotlin kutuphanesi
 #Görselleştirme kütüphanesi
@@ -63,3 +62,111 @@ df1.plot(grid = True, linestyle = ':')  # lineları noktalar haline dönüştür
 
 df1.plot(grid=True, alpha=0.1)   #lineları saydamlaştırır
 #plt.show()
+
+#%% Scatter Plot
+
+setosa = df[df.Species == "Iris-setosa"]   # eğer setosa = df.Species=="Iris-setosa"  dersek bize sadece true-false lardan oluşan tabloyu gösteriyor
+versicolor = df[df.Species == "Iris-versicolor"]
+virginica = df[df.Species == "Iris-virginica"]
+
+plt.scatter(setosa.PetalLengthCm, setosa.PetalWidthCm, color ="red", label = "setosa")
+plt.scatter(versicolor.PetalLengthCm, versicolor.PetalWidthCm, color ="blue", label = "versicolor")
+plt.scatter(virginica.PetalLengthCm, virginica.PetalWidthCm, color ="green", label = "virginica")
+plt.legend()
+plt.xlabel("PetalLengthCm")
+plt.ylabel("PetalWidthCm")
+plt.title("scatter plot")
+plt.show()
+
+#%% Histogram
+
+plt.hist(setosa.PetalLengthCm, bins = 10)   # bin barların sayısını ifade eder
+plt.xlabel("PetalLEngthCm Values")
+plt.ylabel("frekans")
+plt.title("hist")
+plt.show()
+
+#%% Bar Plot
+
+import numpy as np
+
+# x = np.array([1,2,3,4,5,6,7])
+
+# y  = x*2 + 5
+
+# plt.bar(x,y)
+# plt.title("Bar plot")
+# plt.xlabel("X")
+# plt.ylabel("Y")
+# plt.show()
+
+x = np.array([1,2,3,4,5,6,7])
+a = ["turkey", "usa", "a", "b", "c", "d","w"]
+
+y  = x*2 + 5  # burayı gelir dağılımı gibi düşünelim
+
+plt.bar(a,y)
+plt.title("Bar plot")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.show()
+
+#%% Subplots
+
+df1.plot(grid=True, alpha=0.9, subplots = True) 
+plt.show()
+
+setosa = df[df.Species == "Iris-setosa"]   # eğer setosa = df.Species=="Iris-setosa"  dersek bize sadece true-false lardan oluşan tabloyu gösteriyor
+versicolor = df[df.Species == "Iris-versicolor"]
+virginica = df[df.Species == "Iris-virginica"]
+
+plt.subplot(2,1,1)   # 2 grafikten oluşur ve 1. column 1. satırı ifade eder
+plt.plot(setosa.Id, setosa.PetalLengthCm, color = "red", label = "setosa")  # çizdirme işleminde kullandığımız değişkenler
+plt.ylabel("setosa.PetalLengthCm")
+
+plt.subplot(2,1,2)   #2 grafikten oluşur ve 1. column 2. satırı ifade eder
+plt.plot(versicolor.Id, versicolor.PetalLengthCm, color = "green", label = "versicolor")
+plt.ylabel("versicolor.PetalLengthCm")
+
+plt.show()
+
+#%% QUIZ NOTLAR
+
+import numpy as np
+
+array1 = np.array([[1,2,3,4,5],[6,7,8,9,10]])
+
+print(array1[-1,:])   #
+
+array = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+print(array.reshape(3,3)) 
+
+a = np.array([1,2,3])
+
+print(a.sum()) 
+
+print(a.max()) 
+
+print(a.min()) 
+
+print(np.linspace(10,15,5)) 
+
+import pandas as pd
+
+
+dictionary = {"NAME":["ali","veli","kenan","hilal","ayse","evren"],
+
+              "AGE":[15,16,17,33,45,66],
+
+              "MAAS": [100,150,240,350,110,220]}
+
+dataFrame1 = pd.DataFrame(dictionary)
+
+ortalama_maas = dataFrame1.MAAS.mean()
+
+s = np.sum([True if ortalama_maas > each else False for each in dataFrame1.MAAS])
+
+print(s)
+print(dataFrame1.describe())
+c = dataFrame1.iloc[:,2]
